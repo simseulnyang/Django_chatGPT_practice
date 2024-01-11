db974c3dd1716e7523c4466ebdccf7c0c8994615
simseulnyang <happysseul627@gmail.com>
2024-01-11 22:11:34 +0900

    :sparkles:Feat : form 및 view 코드 추가

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,20 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
 from .models import RolePlayingRoom
 from .forms import RolePlayingRoomForm
 
 
+@method_decorator(staff_member_required, name="dispatch")
 class RolePlayingRoomCreateView(CreateView):
     model = RolePlayingRoom
     form_class = RolePlayingRoomForm
+
+    def form_vaild(self, form):
+        role_playing_room = form.save()
+        role_playing_room.user = self.request.user
+        return super().form_valid(form)
 
 
 role_playing_room_new = RolePlayingRoomCreateView.as_view()
