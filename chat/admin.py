ed235044436bfb2a90d28078301bc2c93656232a
simseulnyang <happysseul627@gmail.com>
2024-01-11 22:47:44 +0900

    :sparkles:Feat : admin page를 통한 저장 및 확인

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .forms import RolePlayingRoomForm
+from .models import RolePlayingRoom
+
+
+@admin.register(RolePlayingRoom)
+class RolePlayingRoomAdmin(admin.ModelAdmin):
+    form = RolePlayingRoomForm
+
+    # save request.user if form is valid
+    def save_model(self, request, obj, form, change):
+        # 신규 생성에서만 reqeust.user를 할당
+        if change is False and form.is_valid():
+            obj.user = request.user
+
+        super().save_model(request, obj, form, change)
