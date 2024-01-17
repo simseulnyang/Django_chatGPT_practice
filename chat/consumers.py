ae8ca701944e5a3fbff425aa1c7558acdfe50096
simseulnyang <happysseul627@gmail.com>
2024-01-17 19:54:09 +0900

    :sparkles: Feat : 웹소켓 통신 준비

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,8 +2,11 @@ from channels.generic.websocket import JsonWebsocketConsumer
 
 
 class RolePlayingConsumer(JsonWebsocketConsumer):
-    # def connect(self):
-    #     self.accept()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.count = 0
 
     def receive_json(self, content, **kwargs):
+        self.count += 1
+        content['count'] = self.count
         self.send_json(content)
